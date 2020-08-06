113f2458224319a932e6c66d6f04eee5c6c8456e
Streeterxs <afonso_mg3@hotmail.com>
2020-08-06 18:33:39 -0300

    Stronghold - Basic proxy setup.

--- projects/Stronghold/src/app.ts
+++ projects/Stronghold/src/app.ts
@@ -1,11 +1,36 @@
-import koa from 'koa';
+import koa, { Context } from 'koa';
 import helmet from 'koa-helmet';
 import logger from 'koa-logger';
+import httpProxy from 'http-proxy';
 
 import {appLogger} from './appLogger';
 
+const log = appLogger.extend('entry');
+const proxy = httpProxy.createProxyServer();
 const app = new koa();
 app.use(helmet());
 app.use(logger());
+
+function webProxy(context: Context, options: httpProxy.ServerOptions) {
+
+    return new Promise((resolve, reject) => {
+        context.res.on('finish', resolve);
+        proxy.web(context.req, context.res, options, reject);
+    });
+};
+
+app.use(async (context, next) => {
+    try {
+
+        log('context: ', context);
+        await webProxy(context, {target: 'http://localhost:3333'});
+    } catch (error) {
+
+        log('error: ', error);
+    } finally {
+
+        next();
+    }
+});
 
 export default app;
