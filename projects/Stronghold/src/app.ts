beb0e72e7383e576f0f5af38be79a4a3ac406d11
Streeterxs <afonso_mg3@hotmail.com>
2020-08-08 17:47:34 -0300

    PersonsSector - Created a new project to manage users

--- projects/Stronghold/src/app.ts
+++ projects/Stronghold/src/app.ts
@@ -1,6 +1,7 @@
 import koa, { Context } from 'koa';
 import helmet from 'koa-helmet';
 import logger from 'koa-logger';
+import cors from 'kcors';
 
 import {appLogger} from './appLogger';
 import router from './routes';
@@ -9,6 +10,7 @@ const log = appLogger.extend('entry');
 const app = new koa();
 app.use(helmet());
 app.use(logger());
+app.use(cors());
 
 app.use(router.routes());
 export default app;
