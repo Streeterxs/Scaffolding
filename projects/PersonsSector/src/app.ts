3ddb5a6580f44ecb4180bf95ca29f6fc1a7606e0
Streeterxs <afonso_mg3@hotmail.com>
2020-08-10 14:37:00 -0300

    PersonsSector - OAuth2: Base setup mongoose models for oauth2-server

--- projects/PersonsSector/src/app.ts
+++ projects/PersonsSector/src/app.ts
@@ -3,16 +3,35 @@ import Router from 'koa-router';
 import logger from 'koa-logger';
 import { GraphQLError } from 'graphql';
 import graphqlHttp from 'koa-graphql';
+import OAuth2 from 'oauth2-server';
 
 import { appLogger } from './appLogger';
 import Schema from './schema';
+import { User, OAuthTokens, OAuthClient } from './modules/user/UserModel';
 
 const log = appLogger.extend('entry');
 
+const model:
+    OAuth2.AuthorizationCodeModel |
+    OAuth2.ClientCredentialsModel |
+    OAuth2.RefreshTokenModel |
+    OAuth2.PasswordModel |
+    OAuth2.ExtensionModel = {
+        getAccessToken: OAuthTokens.getAccessToken,
+        saveToken: OAuthTokens.saveToken,
+        verifyScope: OAuthTokens.verifyScope,
+        getRefreshToken: OAuthTokens.getRefreshToken,
+        getClient: OAuthClient.getClient,
+        getUser: User.getUser
+    };
+const oauth = new OAuth2({
+    model
+});
 const router = new Router();
 const app = new koa();
 
 app.use(logger());
+const user = new User();
 
 export const graphqlSettings = async (req: any) => {
 
