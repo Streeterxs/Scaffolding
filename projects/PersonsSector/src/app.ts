d002e0014644f8484ccc2c8a737adad88a8f8d7f
Streeterxs <afonso_mg3@hotmail.com>
2020-08-13 11:49:33 -0300

    PersonsSector - Added koaOauthServer Module.

--- projects/PersonsSector/src/app.ts
+++ projects/PersonsSector/src/app.ts
@@ -9,6 +9,7 @@ import { appLogger } from './appLogger';
 import Schema from './schema';
 import { User, OAuthTokens, OAuthClient } from './modules/user/UserModel';
 import { createServer } from 'http';
+import { koaOauhServer } from './koaOauhServer';
 
 const log = appLogger.extend('entry');
 
@@ -28,116 +29,17 @@ const model:
         getClient: OAuthClient.getClient,
         getUser: User.getUser
     };
-const oauth = new OAuth2({
+const router = new Router();
+const app = koaOauhServer({
     model
 });
-const router = new Router();
-const app = new koa();
-// https://stackoverflow.com/questions/45782303/best-way-to-add-helper-methods-to-context-object-in-koa2
-app.context.oauth = oauth;
-app.context.authenticate = function() {
-
-    return (next) => {
-
-        log('this: ', this);
-        log('this.request: ', this.request);
-        const request = new Request(this.request);
-        const response = new Response(this.response);
-
-    try {
-        this.state.oauth = {
-            token: this.oauth.authenticate(request, response)
-        };
-    } catch (e) {
-
-        log('error: ', e);
-        if (e instanceof UnauthorizedRequestError) {
-            this.status = e.code;
-        } else {
-            this.body = { error: e.name, error_description: e.message };
-            this.status = e.code;
-        }
-
-        return this.app.emit('error', e, this);
-    }
-
-      return next;
-    };
-};
-app.context.authorize = function() {
-
-    return async (next) => {
-        const request = new Request(this.request);
-        const response = new Response(this.response);
-
-        try {
-            this.state.oauth = {
-                code: await this.oauth.authorize(request, response)
-            };
-
-            this.body = response.body;
-            this.status = response.status;
-
-            this.set(response.headers);
-        } catch (e) {
-            if (response) {
-                this.set(response.headers);
-            }
-
-            if (e instanceof UnauthorizedRequestError) {
-                this.status = e.code;
-            } else {
-                this.body = { error: e.name, error_description: e.message };
-                this.status = e.code;
-            }
-
-            return this.app.emit('error', e, this);
-        }
-
-        return next;
-    };
-};
-app.context.token = function() {
-
-    return async (next) => {
-        const request = new Request(this.request);
-        const response = new Response(this.response);
-
-        try {
-            this.state.oauth = {
-                token: await this.oauth.token(request, response)
-            };
-
-            this.body = response.body;
-            this.status = response.status;
-
-            this.set(response.headers);
-        } catch (e) {
-            if (response) {
-                this.set(response.headers);
-            }
-
-            if (e instanceof UnauthorizedRequestError) {
-                this.status = e.code;
-            } else {
-                this.body = { error: e.name, error_description: e.message };
-                this.status = e.code;
-            }
-
-            return this.app.emit('error', e, this);
-        }
-
-        return next;
-    };
-};
 
 app.use(async (context, next) => {
     const teste = await context.authenticate();
     log('context.state: ', context.state.oauth);
     log('context.headers: ', context.headers);
     await teste(next);
-    log('teste!!!!!!!!!!!');
-    await next();
+    // await next();
 });
 
 app.use(logger());
