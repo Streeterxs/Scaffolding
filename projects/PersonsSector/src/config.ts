3bbbab1bd948325110ae4489bfde9391db7b8343
Streeterxs <afonso_mg3@hotmail.com>
2020-08-21 12:10:57 -0300

    PersonsSector - Environment Variables: Added Stronghold clientId and clientSecret

--- projects/PersonsSector/src/config.ts
+++ projects/PersonsSector/src/config.ts
@@ -17,9 +17,17 @@ dotenvsafe.config({
 const ENV = process.env;
 
 const config = {
-    db_url: `${ENV.MONGODB_URL}`
+    db_url: `${ENV.MONGODB_URL}`,
 };
 
+export const clients: {id: string, secret: string}[] = [
+    {
+        id: `${ENV.STRONGHOLD_CLIENT_ID}`,
+        secret: `${ENV.STRONGHOLD_CLIENT_SECRET}`
+    },
+];
+
 log('config: ', config);
+log('clients: ', clients);
 
 export default config;
