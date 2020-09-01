6729bf4c90fc914afc3bbf357e3e6fa23c2a36d2
Streeterxs <afonso_mg3@hotmail.com>
2020-09-01 16:11:59 -0300

    Stronghold - PersonsSector pointing: added clientid, clientsecret and /token route to request PersonsSector /token route for user credentials. Created basicAuth middleware to check if the body is correct and setup basic authorization

--- projects/Stronghold/src/routes.ts
+++ projects/Stronghold/src/routes.ts
@@ -1,18 +1,33 @@
 import Router from 'koa-router';
+import fetch from 'node-fetch';
 
 import bookManager, {graphqlHttpServer} from '@BookScaffolding/bookmanager';
-import { appRouter as PersonSector, authenticate, authorize, token } from '@BookScaffolding/personssector';
 
 import { appLogger } from './appLogger';
+import config from './config';
+import { basicAuth } from './middlewares';
 
 const router = new Router();
 const log = appLogger.extend('routes');
 
 log('bookManager: ', bookManager);
 
-router.get('/', authenticate(), (context, next) => {
+router.get('/', (context, next) => {
     context.body = 'helloooo!';
 });
+
+router.post('/token', basicAuth(), async (context, next) => {
+
+        const {grant_type, username, password} = context.request.body;
+        const response = await fetch(`${config.services.personssector.baseurl}/${config.services.personssector.routes[0] /* example */}`, {
+            headers: {...context.headers},
+            body: `grant_type=${grant_type}&username=${username}&password=${password}`,
+            method: 'POST'
+        });
+
+        context.body = await response.json();
+        await next();
+    });
 
 router.all('/bookmanager', graphqlHttpServer);
 
