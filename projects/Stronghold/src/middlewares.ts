c37840baf4f8cc88b7745e71d84d599777bfa262
Streeterxs <afonso_mg3@hotmail.com>
2020-09-02 17:32:17 -0300

    Stronghold - permissionLimiter: added to validate authenticated user permission before permit route

--- projects/Stronghold/src/middlewares.ts
+++ projects/Stronghold/src/middlewares.ts
@@ -1,5 +1,8 @@
 import { ParameterizedContext, Next } from "koa";
 import Router from "koa-router";
+import fetch from 'node-fetch';
+
+import { permissions } from '@BookScaffolding/personssector';
 
 import { appLogger } from "./appLogger";
 import config from "./config";
@@ -16,12 +19,48 @@ export const basicAuth = () => {
 
         if (canProceed) {
 
-            context.req.headers = {...context.req.headers, Authorization: `Basic ${Buffer.from(`${config.credentials.clientId}:${config.credentials.clientSecret}`).toString('base64')}`};
+            context.req.headers = {...context.req.headers, authorization: `Basic ${Buffer.from(`${config.credentials.clientId}:${config.credentials.clientSecret}`).toString('base64')}`};
             log('context.headers', context.headers);
             await next();
         } else {
 
             throw new Error("Couldn't login");
+        }
+    };
+};
+
+export const permissionLimiter = (...args: [permissions, permissions?, permissions?, permissions?]) => {
+
+    const authenticate = async (context: ParameterizedContext<any, Router.IRouterParamContext<any, {}>>): Promise<void> => {
+
+        log('config.services.personssector.routes[1]: ', config.services.personssector.routes[1]);
+
+        const response = await fetch(`${config.services.personssector.baseurl}/${config.services.personssector.routes[1] /* example */}`, {
+            headers: {...context.headers},
+            method: 'POST'
+        });
+
+        const responseReturned = await response.json();
+        log('responseReturned: ', responseReturned);
+        context.state.authenticatedUser = responseReturned;
+    };
+
+    return async (context: ParameterizedContext<any, Router.IRouterParamContext<any, {}>>, next: Next) => {
+
+        await authenticate(context);
+        const { permission } = context.state.authenticatedUser;
+
+        log('user permission: ', permission);
+        const canRequest = args.reduce((acc, actual) => {
+            return acc || (actual === permission)
+        }, false);
+
+        if (canRequest) {
+
+            await next();
+        } else {
+
+            throw new Error('User forbidden');
         }
     };
 };
