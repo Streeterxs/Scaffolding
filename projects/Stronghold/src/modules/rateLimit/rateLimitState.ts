a8c66cdb8a5ef9ebcd84ad074bd4b1c6fbcfd638
Streeterxs <afonso_mg3@hotmail.com>
2020-09-03 16:17:05 -0300

    Stronghold - RateLimit Implementation: Added @authentication/rate-limit package, created mongoose model for rateLimitState

---
+++ projects/Stronghold/src/modules/rateLimit/rateLimitState.ts
@@ -0,0 +1,13 @@
+import Mongoose, { Schema, model,  } from "mongoose";
+import { RateLimitState, RateLimitStore } from '@authentication/rate-limit';
+
+export interface IRateLimitState extends Mongoose.Document, RateLimitState{
+    userID: string;
+};
+const rateLimitStateSchema = new Schema<IRateLimitState>({
+    userID: {type: String, required: true},
+    value: {type: Number, required: true},
+    timestamp: {type: Number, required: true}
+});
+
+export const RateLimitStateModel = model<IRateLimitState, Mongoose.Model<IRateLimitState>>('Stronghold_ExpRateLimit', rateLimitStateSchema);
