3ba7ef499aacc96725d27b9aec0acd271807c30e
Streeterxs <afonso_mg3@hotmail.com>
2020-09-03 16:17:21 -0300

    Stronghold - Rate Limit: rate limit loader

---
+++ projects/Stronghold/src/modules/rateLimit/rateLimitLoader.ts
@@ -0,0 +1,22 @@
+import dataloader from 'dataloader';
+
+import { RateLimitStateModel } from './rateLimitState';
+
+const rateLimitLoader = new dataloader((keys: string[]) => RateLimitStateModel.find({_id: {$in: keys}}));
+
+export const loadRateLimit = async (userId: string) => {
+
+    const rateLimitFinded = await rateLimitLoader.load(userId);
+    return rateLimitFinded;
+};
+
+export const loadManyRateLimit = async (usersId: string[]) => {
+
+    const rateLimitFinded = await rateLimitLoader.loadMany(usersId);
+    return rateLimitFinded;
+};
+
+export const removeRateLimit = async (userID: string) => {
+
+    await RateLimitStateModel.remove({userID});
+};
